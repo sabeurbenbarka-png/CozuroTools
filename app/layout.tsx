be227eb1ac7a1351c09c58ede946f9c5aa62aec1
sabeurbenbarka-png <sabeurbenbarka@gmail.com>
2026-06-08 19:17:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,6 +82,10 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  // هنا قمنا بإضافة كود التحقق من جوجل بشكل احترافي ومتوافق مع Next.js
+  verification: {
+    google: 'google999c899bde6cd386',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
